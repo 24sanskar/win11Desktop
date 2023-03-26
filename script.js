2a01f33462ffa897d822aaa389f3ff25ffbf49b9
24sanskar <105884159+24sanskar@users.noreply.github.com>
2023-03-27 01:23:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let taskbar = document.getElementsByClassName("taskbar")[0]
-let startmenu = document.getElementsByClassName("startmenu")[0]
+let taskbar = document.getElementsByClassName("taskbar")[0];
+let startmenu = document.getElementsByClassName("startmenu")[0];
 
-taskbar.addEventListener("click", ()=>{
+taskbar.addEventListener("click", () => {
+  if (startmenu.style.bottom == "50px") {
+    startmenu.style.bottom = "-655px";
+  } else {
+    startmenu.style.bottom = "50px";
+  }
+});
 
-    if(startmenu.style.bottom == "50px"){
-        startmenu.style.bottom = "-655px"
-    }
-    else{
-        startmenu.style.bottom = "50px"
-    }
-})
-
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let ampm = hours >= 12 ? "PM" : "AM";
+hours = hours % 12;
+hours = hours ? hours : 12;
+minutes = minutes < 10 ? "0" + minutes : minutes;
+let time = hours + ":" + minutes + " " + ampm;
+let dateStr = date.toLocaleDateString();
+document.getElementById("time").textContent = time;
+document.getElementById("date").textContent = dateStr;
